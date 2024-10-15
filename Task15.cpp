a05a46cb38d892a5ff87d0b584ccf68de1c014e3
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-10-16 00:38:25 +0300

    Add case when left number is bigger
    Keep up the good work! The solution is great!

--- Task15.cpp
+++ Task15.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
@@ -8,20 +9,30 @@ int main()
 
 	int firstDigit = n / 1000;
 	n %= 1000;
+
 	int secondDigit = n / 100;
 	n %= 100;
+
 	int thirdDigit = n / 10;
 	n %= 10;
+
 	int fourthDigit = n;
 
 	int firstNum = firstDigit * 10 + fourthDigit;
 	int secondNum = secondDigit * 10 + thirdDigit;
 
-	if (firstNum< secondNum) {
-		cout << "Less";
+	if (firstNum < secondNum)
+	{
+		cout << "Less: (" << firstNum << " < " << secondNum << ")";
 	}
-	else if (firstNum == secondNum) {
-		cout << "Equal";
-		}
+	else if (firstNum == secondNum)
+	{
+		cout << "Equal: (" << firstNum << " = " << secondNum << ")";
+	}
+	else
+	{
+		cout << "Bigger: (" << firstNum << " > " << secondNum << ")";
+	}
 
+	return 0;
 }
