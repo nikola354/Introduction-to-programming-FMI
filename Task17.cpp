9f590c3aa80f0d76bcbcc860184e4ca00ea85b45
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-10-16 00:58:09 +0300

    Update Task17.cpp
    The solution was ok! The logic for union, intersection, difference and if the symbol is in only one set are correct. The only drawbacks were that (given the symbols are case-insensitive) all of the symbols had to be converted to uppercase or lowercase.

--- Task17.cpp
+++ Task17.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 
 int main()
 {
-    char a, b, c, d, e;
-    cin >> a>> b>> c>> d>> e;
-    int first = e >= a && e <= b;
-    int second = e >= c && e <= d;
+	const char CAPITAL_TO_LOWER = 'a' - 'A';
 
-    bool isUnion = first || second;
-    cout << isUnion;
-    bool isIntersection = first && second;
+	char capitalStart, capitalEnd, lowerStart, lowerEnd, symbol;
+	cin >> capitalStart >> capitalEnd >> lowerStart >> lowerEnd >> symbol;
 
-    cout << isIntersection;
-    cout << (first && (!second));
-    cout << (isUnion && (!isIntersection));
+	if (symbol >= 'A' && symbol <= 'Z')
+	{
+		symbol += CAPITAL_TO_LOWER;
+	}
 
+	capitalStart += CAPITAL_TO_LOWER;
+	capitalEnd += CAPITAL_TO_LOWER;
 
-    
+	bool isInFirst = symbol >= capitalStart && symbol <= capitalEnd;
+	bool isInSecond = symbol >= lowerStart && symbol <= lowerEnd;
 
+	bool isUnion = isInFirst || isInSecond;
+	bool isIntersection = isInFirst && isInSecond;
+	bool isDifference = isInFirst && !isInSecond;
+	bool isInOnlySet = isUnion && !isIntersection;
+
+	cout << (isUnion ? "true" : "false") << endl;
+	cout << (isIntersection ? "true" : "false") << endl;
+	cout << (isDifference ? "true" : "false") << endl;
+	cout << (isInOnlySet ? "true" : "false") << endl;
 }
-
-
