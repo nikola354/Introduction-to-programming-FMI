f5b1d47f2aab44baf68594c19c5c25ab5c33b302
Yana Dimitrova <yanadimedu@outlook.com>
2024-10-16 22:34:01 +0300

    3, 10, 14, 15

---
+++ Task 14 i 15 .cpp
@@ -0,0 +1,64 @@
+// Task 14 i 15 .cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	//Задача 14. Създайте калкулатор, поддържащ операциите +, -, *, / на две реални числа.
+	//От конзолата се прочита число, операция и второ число и се извежда резултатът.
+	//
+	// tuk nqmam vuprosi prosto da ne izleze che sum napravila samo tri zadachi i to nepulnocenni
+	// vsichki ot fajla sa gotovi zanapred shte kachvam vsichko ako se nalaga
+	//
+	//int a, b;
+	//char operation;
+	//cin >> a >> operation >> b;
+	//
+	//switch (operation) {
+	//case '+':
+	//	cout << a + b << endl;
+	//	break;
+	//case '*':
+	//	cout << a * b << endl;
+	//	break;
+	//case '-':
+	//	cout << a - b << endl;
+	//	break;
+	//case '/':
+	//	cout << a / b << endl;
+	//	break;
+	//default: cout << "invalid input" << endl;
+	//}
+
+
+	//Задача 15. Да се състави програма, чрез която се въвежда 4-цифренo естествено число от интервала [1000.. 9999]. 
+	//От това число се формират 2 нови 2-цифрени числа. Първото число се формира от 1-та и 4-та цифра на въведеното число. 
+	//Второто число се формира от 2-рa - 3-та цифра на въведеното число. 
+	//На екрана да се изведе дали 1-то ново число e по-малко <, равно = или по-голямо от 2-то число.
+	//
+	// ???????????????????? po odbur nachin za poluchavane na newnum1&2 ????????????????????????????????????????????????????????
+	//
+	//int num;
+	//cin >> num;
+	//int newnum1, newnum2;
+	////ot 9ta polzvam <3     ((num / 1000 == num % 10) && (((num / 100) % 10) == ((num / 10) % 10)))
+	//newnum1 = ( (num / 1000) * 10 + num % 10 );
+	//newnum2 = ((((num / 100) % 10) * 10) + ((num / 10) % 10));
+	//
+	//if (newnum1 < newnum2) {
+	//	cout << "less" << endl;
+	//}
+	//else if (newnum1 = newnum2) {
+	//	cout << "equal" << endl;
+	//}
+	//else  {
+	//	cout << "greater" << endl;
+	//}
+
+
+	// drugite v chas
+	//dva chasa e lqgam si
+}
+
+
