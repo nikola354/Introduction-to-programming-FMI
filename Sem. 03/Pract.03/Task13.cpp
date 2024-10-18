78aaa82585af1b36c47f21d51297f068a19d8477
momchil70 <117820041+momchil70@users.noreply.github.com>
2024-10-18 20:38:24 +0300

    Update Task13.cpp

--- Sem. 03/Pract.03/Task13.cpp
+++ Sem. 03/Pract.03/Task13.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 int main() {
-    int num,n,k,sum,maxDifference;
-    std::cin>>num>>n>>k;
-    if(n>k)maxDifference=n-k
-    else maxDifference = k-n;
+    int num, n, k, sum, maxDifference = 0;
+    std::cin >> num;
+    if (num == 0) return 0;
+    std::cin >> n >> k;
+
+    if (n > k) maxDifference = n - k;
+    else maxDifference = k - n;
+
     sum = n + k;
-    bool isEqual=true;
-    for(int i=1;i<num;i++){
-        int n,k,localDifference;
-        std::cin>>n>>k;
-        if(sum != n+k){
-            if(n>k)localDifference = n - k;
-            else localDifference = k - n;
-            isEqual=false;
-            if(localDifference > maxDifference) maxDifference = localDifference;
+    bool isEqual = true;
+
+    for (int i = 1; i < num; i++) {
+        int first, second, currentDifference = 0;
+        std::cin >> first >> second;
+
+        if (sum != first + second) {
+            if (first > second) currentDifference = first - second;
+            else currentDifference = second - first;
+
+            isEqual = false;
+            if (currentDifference > maxDifference) maxDifference = currentDifference;
         }
     }
-    if(isEqual) std::cout<<"Equal sums: "<<sum;
-    else std::cout<<"Different sums. Max Difference: "<<maxDifference;
+
+    if (isEqual) std::cout << "Equal sums: " << sum;
+    else std::cout << "Different sums. Max Difference: " << maxDifference;
 }
