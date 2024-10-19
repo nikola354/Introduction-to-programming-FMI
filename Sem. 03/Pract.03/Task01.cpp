d45fee3b20c1647726a4d57a4037246aaab5054d
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2024-10-19 23:33:24 +0300

    Task01.cpp

--- Sem. 03/Pract.03/Task01.cpp
+++ Sem. 03/Pract.03/Task01.cpp
@@ -3,7 +3,7 @@ int main() {
     int n,m;
     std::cin>>n>>m;
     if(n<0 || m > 255 || n > 255 || (n>m) || m<0){
-        std::cout<<"Invalid input";
+        std::cout<<"Invalid codes";
         return 0;
     }
     for(int i=n;i<=m;i++){
