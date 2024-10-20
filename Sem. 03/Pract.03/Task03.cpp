270f0f7c203c44be0fbbf0a5eb3b6fc9d364207c
momchil70 <117820041+momchil70@users.noreply.github.com>
2024-10-21 00:28:18 +0300

    Update Task03.cpp

--- Sem. 03/Pract.03/Task03.cpp
+++ Sem. 03/Pract.03/Task03.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 int main() {
-    int n,factorialOfN=1;
+    int n,a=0,b=1,sum;
     std::cin>>n;
-    while(n!=0){
-        factorialOfN *=n;
-        n--;
+    for(int i=0;i<n;i++){
+        if(i==0){
+            std::cout<<a<<" ";
+        }
+        else if(i==1){
+            std::cout<<b<<" ";
+        }else{
+            sum = a + b;
+            a = b;
+            b = sum;
+            std::cout<<sum<<" ";
+        }
     }
-    std::cout<<factorialOfN;
 }
