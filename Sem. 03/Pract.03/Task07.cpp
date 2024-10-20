1f0eca7bb00d61c0c15a2a8355a51d5aa454d18d
momchil70 <117820041+momchil70@users.noreply.github.com>
2024-10-21 00:36:15 +0300

    Update Task07.cpp

--- Sem. 03/Pract.03/Task07.cpp
+++ Sem. 03/Pract.03/Task07.cpp
@@ -2,9 +2,8 @@
 int main() {
     int n,sum=0;
     std::cin>>n;
-    for(int i=1;i<n;i++){
-        if(n%i==0)sum+=i;
+    for (int i=1;i<n;i++) {
+        if (n%i==0) sum+=i;
     }
-    if(n==sum) std::cout<<true;
-    else std::cout<<false;
+    std::cout<<(sum==n)<<std::endl;
 }
