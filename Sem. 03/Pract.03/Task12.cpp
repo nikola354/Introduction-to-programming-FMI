f69c43e3ff7389befdf0ab6e42da96cbd890c6ff
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-10-20 03:02:31 +0300

    Update Task12.cpp

--- Sem. 03/Pract.03/Task12.cpp
+++ Sem. 03/Pract.03/Task12.cpp
@@ -1,28 +1,37 @@
-// Създайте програма, която приема естествено число N и след това чете неопределен 
-// брой цели положителни числа от стандартния вход. Въвеждането се прекратява при 
-// прочитане на числото -1. Да се отпечата сборът на всички числа от въведените, 
-// които са взаимно прости с N.
 #include <iostream>;
 
 int main() {
 	int n, k = 0, sum = 0;
-	std::cin >> n;
+	std::cin >> n >> k;
 	
-	do
+	while (k != -1)
 	{
-		std::cin >> k;
+		int first = n;
+		int second = k;
 
-		// could be made into a separate method
-		int gcd = n > k ? k : n;
-		while (gcd > 1) {
-			if (n % gcd == 0 && k % gcd == 0)
-				break;
-			gcd--;
+		if (first < second)
+		{
+			int temp = first;
+			first = second;
+			second = temp;
 		}
 
-		if (gcd == 1)
-			sum += k;
+		while (second != 0)
+		{
+			int mod = first % second;
+			first = second;
+			second = mod;
+		}
 
-	} while (k != -1);
-	std::cout << sum << '\n';
+		if (first == 1)
+		{
+			sum += k;
+		}
+
+		std::cin >> k;
+	}
+
+	std::cout << sum << std::endl;
+
+	return 0;
 }
