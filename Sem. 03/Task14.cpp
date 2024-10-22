1174057acc6173e8b76965a83864095659d54581
MartinUzunov1707 <muzun1707@gmail.com>
2024-10-22 20:23:37 +0300

    Add solution to task 14.
    Add solution to task 14 from week 2.

---
+++ Sem. 03/Task14.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//int main()
+//{
+//	int n,sum = 0,max,current;
+//	std::cin >> n;
+//	for (int i = 0;i < n;i++) {
+//		std::cin >> current;
+//		if (i == 0 || current > max) {
+//			max = current;
+//		}
+//		sum += current;
+//	}
+//	if (max == sum - max) {
+//		std::cout << "YES";
+//	}
+//	else {
+//		std::cout << "NO";
+//	}
+//}
