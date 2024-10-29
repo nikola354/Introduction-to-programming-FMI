d6806abd8b4ded48274f01f876a463eb5a8737b3
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2024-10-29 19:08:14 +0200

    Update week4_pract_Task10.cpp

--- Sem. 04/Pract.04/week4_pract_Task10.cpp
+++ Sem. 04/Pract.04/week4_pract_Task10.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 int reverse(int num){
     int reversed = 0;
-    while(num!=0){
+    while(num != 0){
         reversed = reversed*10 + num%10;
-        num/=10;
+        num /= 10;
     }
     return reversed;
 }
-int kThNumber(int reversed,int k){
-    int digit;
-    while(k-1!=0){
-        reversed/=10;
+int kThNumber(int n,int k){
+     n = reverse(n);
+    while(k - 1 != 0){
+        n /= 10;
         k--;
     }
-    return reversed%10;
+    return n % 10;
 }
 int main() {
     int n,k;
     std::cin>>n>>k;
-    std::cout<<kThNumber(reverse(n),k);    
+    std::cout<<kThNumber(n,k);    
 
     return 0;
 }
