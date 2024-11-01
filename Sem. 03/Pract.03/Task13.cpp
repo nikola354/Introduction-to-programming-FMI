8617c6adeb48a5a58bcac671f604520f4f498143
NoraKirilova <norakirilova.work@gmail.com>
2024-11-01 20:54:27 +0200

    Task13.cpp

--- Sem. 03/Pract.03/Task13.cpp
+++ Sem. 03/Pract.03/Task13.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-int main() {
-    int num, n, k, sum, maxDifference = 0;
-    std::cin >> num;
-    if (num <= 0) return 0;
-    std::cin >> n >> k;
+using namespace std;
+int main()
+{
+int num, n, k, sum, maxDifference = 0;
+cin >> num;
+if (num <= 0) return 0;
+cin >> n >> k;
+sum = n + k;
+bool isEqual = true;
 
-    if (n > k) maxDifference = n - k;
-    else maxDifference = k - n;
-
-    sum = n + k;
-    bool isEqual = true;
-
-    for (int i = 1; i < num; i++) {
-        int first, second, currentDifference = 0;
-        std::cin >> first >> second;
-
-        if (sum != first + second) {
-            if (first > second) currentDifference = first - second;
-            else currentDifference = second - first;
-
-            isEqual = false;
-            if (currentDifference > maxDifference) maxDifference = currentDifference;
-        }
-    }
-
-    if (isEqual) std::cout << "Equal sums: " << sum;
-    else std::cout << "Different sums. Max Difference: " << maxDifference;
+for (int i = 1; i < num; i++) {
+	int first, second;
+	cin >> first >> second;
+	int sum1 = first + second;
+	if (sum != sum1) {
+		isEqual = false;
+		if (sum > sum1) {
+			maxDifference = sum - sum1;
+		}
+		else {
+			maxDifference = sum1 - sum;
+		}
+		sum = sum1;
+	}
+}
+if (isEqual) {
+	cout << "Equal sums: " << sum;
+}
+else {
+	cout << "Different sums. Max Difference: " << maxDifference;
+}
 }
