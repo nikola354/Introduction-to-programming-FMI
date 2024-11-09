ca99c39ad7a190ef02314d74b1715274b0486865
Miroslav Balev <zevs8@abv.bg>
2024-11-09 09:58:09 +0200

    Add Task 14

---
+++ Sem. 06/Pract.06/Task14.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+void mergeArrays(const int[], int, const int[], int, int[], int &);
+
+void printArray(const int[], int);
+
+int main() {
+    int arr1[4] = { 1, 5, 7, 8 };
+    int arr2[3] = { 2, 3, 6 };
+    int result[MAX_SIZE];
+
+    int size3 = 0;
+
+    mergeArrays(arr1, 4, arr2, 3, result, size3);
+    printArray(result, size3);
+
+    return 0;
+}
+
+void mergeArrays(const int arr1[], int size1, const int arr2[], int size2, int result[], int & size3) {
+    int i = 0, j = 0;
+
+    while (i < size1 && j < size2) {
+        if (arr1[i] > arr2[j]) {
+            result[size3] = arr2[j];
+            size3++;
+            j++;
+        } else {
+            result[size3] = arr1[i];
+            size3++;
+            i++;
+        }
+    }
+
+    while (i < size1) {
+        result[size3] = arr1[i];
+        i++;
+        size3++;
+    }
+    
+    while (j < size2) {
+        result[size3] = arr2[j];
+        j++;
+        size3++;
+    }
+}
+
+void printArray(const int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
