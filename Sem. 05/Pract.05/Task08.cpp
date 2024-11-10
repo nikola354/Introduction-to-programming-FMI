6ce3f456e8f2484d1fc787048bbfbedfe0526f77
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2024-11-10 19:59:30 +0200

    Update Task08.cpp

--- Sem. 05/Pract.05/Task08.cpp
+++ Sem. 05/Pract.05/Task08.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
-int digitsInNum(int num){
+
+int digitsInNum(int num) {
     int count = 0;
-    while(num!=0){
+
+    while (num != 0) {
         count++;
         num /= 10;
     }
+
     return count;
 }
-int sumOfDigitsInNum(int num){
+
+int sumOfDigitsInNum(int num) {
     int sum = 0;
-    while(num!=0){
+
+    while (num != 0) {
         sum += num % 10;
         num /= 10;
     }
+
     return sum;
 }
-int aritmeticMean(int num){
-    return sumOfDigitsInNum(num)/digitsInNum(num);
+
+int aritmeticMean(int num) {
+    return sumOfDigitsInNum(num) / digitsInNum(num);
 }
-bool aritmeticMeanGreaterOrEqual(int num, int k){
-    if(aritmeticMean(num) >= k)return true;
-    return false;
+
+bool aritmeticMeanGreaterOrEqual(int num, int k) {
+    return aritmeticMean(num) >= k;
 }
-void run(){
-  int m,n,k;
-  std::cin>>m>>n>>k;
-  for(int i = m;i <= n;i++){
-      if(aritmeticMeanGreaterOrEqual(i,k)) std::cout<<i<<" ";
-  }
+
+void run() {
+    int m, n, k;
+    std::cin >> m >> n >> k;
+    
+    for (int i = m; i <= n; i++) {
+        if (aritmeticMeanGreaterOrEqual(i, k)) std::cout << i << " ";
+    }
 }
+
 int main() {
- run();
+    run();
 }
+
