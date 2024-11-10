96d06512f8f9684f195318bc9fb41646eeeaee0c
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2024-11-10 19:55:48 +0200

    Update Task10.cpp

--- Sem. 05/Pract.05/Task10.cpp
+++ Sem. 05/Pract.05/Task10.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
-int powOfTen(int k){
-    int powerOfTen=1;
-    while(k!=0){
+
+int powOfTen(int k) {
+    int powerOfTen = 1;
+    
+    while (k != 0) {
         powerOfTen *= 10;
         k--;
     }
+    
     return powerOfTen;
 }
-int numbersLengthOfb(int b){
-    int length=0;
-    while(b!=0){
+
+int numbersLengthOfb(int b) {
+    int length = 0;
+    
+    while (b != 0) {
         length++;
         b /= 10;
     }
+    
     return length;
 }
-void newNumbers(int& a, int& b, int k){
-    int tempLastKDigits = a%powOfTen(k);
+
+void newNumbers(int &a, int &b, int k) {
+    int tempLastKDigits = a % powOfTen(k);
+    
     b = tempLastKDigits * powOfTen(numbersLengthOfb(b)) + b;
     a /= powOfTen(k);
-   
 }
+
 int main() {
-  int a,b,k;
-  std::cin>>a>>b>>k;
-  newNumbers(a,b,k);
-  std::cout<<a<<" "<<b;
+    int a, b, k;
+    std::cin >> a >> b >> k;
+    
+    newNumbers(a, b, k);
+    
+    std::cout << a << " " << b;
 }
