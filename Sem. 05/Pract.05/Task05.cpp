353c4ad336ec1d2ae636decd81b0f7de4795d96b
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-11-23 01:59:25 +0200

    Update Task05.cpp

--- Sem. 05/Pract.05/Task05.cpp
+++ Sem. 05/Pract.05/Task05.cpp
@@ -1,49 +1,70 @@
 #include <iostream>
-using namespace std;
-void setkthdigit(int& number, int k, int newdigit){
-    int digit,reversed=0;
-    for(int i=1;i<=k;i++){
-        digit=number%10;
-        number/=10;
-        if(i==k) digit=newdigit;
-        reversed=reversed*10+digit;
-       }
-       while(reversed>0){
-        digit=reversed%10;
-        number=number*10+digit;
-        reversed/=10;
-       }
-    }
 
-int findk(int number,int k){
-    int a,n=number;
-    for(int i=1;i<=k;i++){
-    a=n%10;
-    n/=10;
-    }
-    return a;
+using namespace std;
+
+void concat(int& n, int digit) {
+	n = n * 10 + digit;
 }
-void reverse(int& number){
-int reversednum=0;
-int digit;
-int n=number;
-while(n>0){
-    digit=n%10;
-    reversednum=reversednum*10+digit;
-    n/=10;
-}number=reversednum;
+
+void setKthdigit(int& number, int k, int newDigit) {
+	int digit, reversed = 0;
+
+	for (int i = 1; i <= k; i++) {
+		digit = number % 10;
+		number /= 10;
+		if (i == k) digit = newDigit;
+		concat(reversed, digit);
+	}
+
+	while (reversed != 0) {
+		digit = reversed % 10;
+		concat(number, digit);
+		reversed /= 10;
+	}
 }
-void swap(int& m,int& n, int k){
-    reverse(m);reverse(n);
-    int km=findk(m,k);
-    int kn=findk(n,k);
-    setkthdigit(n,k,km);setkthdigit(m,k,kn);
-    reverse(m);reverse(n);
-    
+
+int findK(int number, int k) {
+	for (int i = 1; i < k; i++) {
+		number /= 10;
+	}
+
+	return number % 10;
 }
-int main(){
-    int m,n,k;
-    cin>>m>>n>>k;
-    swap(m,n,k);
-    cout<<m<<" "<<n;
+
+void reverse(int& number) {
+	int reversedNum = 0;
+	int n = number;
+
+	while (n != 0) {
+		int digit = n % 10;
+		concat(reversedNum, digit);
+		n /= 10;
+	}
+	
+	number = reversedNum;
+}
+
+void swapDigits(int& m, int& n, int k) {
+	reverse(m); 
+	reverse(n);
+
+	int km = findK(m, k);
+	int kn = findK(n, k);
+
+	setKthdigit(n, k, km); 
+	setKthdigit(m, k, kn);
+
+	reverse(m); 
+	reverse(n);
+}
+
+int main() {
+	int m, n, k;
+	cin >> m >> n >> k;
+
+	swapDigits(m, n, k);
+
+	cout << m << " " << n;
+
+	return 0;
 }
