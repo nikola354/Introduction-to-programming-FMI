e58c6e965fb1bddda50e4f659038e09aa04896c1
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-11-23 01:41:05 +0200

    Update Task11.cpp

--- Sem. 05/Pract.05/Task11.cpp
+++ Sem. 05/Pract.05/Task11.cpp
@@ -1,22 +1,22 @@
-// Задача 11: Дата представяме като три числа от тип unsigned. 
-// Примерно (4 / 11 / 2022) е валидна дата. Напишете функция, 
-// която приема дата, представена като три числа, и намира следващия ден. 
-// От вас се очаква да проверите дали датата е валидна.
-// Да се напише програма която намира коя ще е датата след n дена.
-
 #include <iostream>
 
 bool isLeapYear(unsigned year);
 unsigned getDaysInMonth(unsigned month, unsigned year);
 bool isDateValid(unsigned day, unsigned month, unsigned year);
+void goToNextDay(unsigned int& day, unsigned int& month, unsigned int& year);
 void printDateAfterNDays(unsigned day, unsigned month, unsigned year, unsigned daysToAdd);
 
-int main(){
-    std::cout << "Enter date in format dd//mm//yyyy and then days, which will be added to it: ";
-    unsigned day, month, year, n;
-    std::cin >> day >> month >> year >> n;
-    std::cout << "\n";
-    printDateAfterNDays(day, month, year, n);
+int main() {
+	std::cout << "Enter date in format dd//mm//yyyy and then days, which will be added to it: ";
+
+	unsigned day, month, year, n;
+	std::cin >> day >> month >> year >> n;
+
+	std::cout << std::endl;
+
+	printDateAfterNDays(day, month, year, n);
+
+	return 0;
 }
 
 bool isLeapYear(unsigned year) {
@@ -24,10 +24,13 @@ bool isLeapYear(unsigned year) {
 }
 
 unsigned getDaysInMonth(unsigned month, unsigned year) {
-	if (month == 2)
+	if (month == 2) {
 		return isLeapYear(year) ? 29 : 28;
-	if (month == 4 || month == 6 || month == 9 || month == 11)
+	}
+	if (month == 4 || month == 6 || month == 9 || month == 11) {
 		return 30;
+	}
+
 	return 31;
 }
 
@@ -36,21 +39,34 @@ bool isDateValid(unsigned day, unsigned month, unsigned year) {
 	return day >= 1 && day <= getDaysInMonth(month, year);
 }
 
+void goToNextDay(unsigned int& day, unsigned int& month, unsigned int& year)
+{
+	if (!isDateValid(day, month, year)) return;
+
+	day++;
+
+	if (day > getDaysInMonth(month, year))
+	{
+		day = 1;
+
+		(month == 12) ? month = 1 : month++;
+
+		if (month == 1) year++;
+	}
+}
+
 void printDateAfterNDays(unsigned day, unsigned month, unsigned year, unsigned daysToAdd) {
-	if (!isDateValid(day, month, year)) { 
+	if (!isDateValid(day, month, year)) {
 		std::cout << "This date is invalid!";
 		return;
 	}
 
-	day += daysToAdd;
-	while (day > getDaysInMonth(month, year)) {
-		day -= getDaysInMonth(month, year);
-		month++;
+	int copyNDays = daysToAdd;
 
-		if (month > 12) {
-			month = 1;
-			year++;
-		}
+	while (copyNDays != 0)
+	{
+		goToNextDay(day, month, year);
+		copyNDays--;
 	}
 
 	std::cout << "Date after " << daysToAdd << " days: "
