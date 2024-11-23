9aa35c56ce5cf4cde5bea707ba1db96cec3bbb22
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-11-23 02:01:41 +0200

    Update Task06.cpp

--- Sem. 05/Pract.05/Task06.cpp
+++ Sem. 05/Pract.05/Task06.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
+
 using namespace std;
-void an(double& a,int n){
-    double c;
-    for(int i=1;i<=n;i++){
-        c=(a*3)/2;
-        a=c;
-    }
+
+void an(double& a, int n) {
+	double c;
+
+	for (int i = 1; i <= n; i++) {
+		c = (a * 3) / 2;
+		a = c;
+	}
 }
-bool isRowEvenOrOdd(int n){
-double a=1.0;
-an(a,n);
-int number=a/1;
-return number%2==0;
+
+bool isRowEvenOrOdd(int n) {
+	double a = 1.0;
+
+	an(a, n);
+
+	int number = a;
+	return number % 2 == 0;
 }
-int main(){
-    int n;
-cin>>n;
-if(isRowEvenOrOdd(n))cout<<"is even";
-else cout<<"not even";
+
+int main() {
+	int n;
+	cin >> n;
+
+	if (isRowEvenOrOdd(n)) {
+		cout << "is even";
+	}
+	else {
+		cout << "not even";
+	}
+
+	return 0;
 }
