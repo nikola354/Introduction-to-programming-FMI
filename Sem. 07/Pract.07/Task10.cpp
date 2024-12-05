480ca1a1e217875d502cf9c9eb77c4e789b050eb
Nikola Manolov <nikola.manolov20@gmail.com>
2024-12-05 11:53:49 +0200

    Update Task10.cpp

--- Sem. 07/Pract.07/Task10.cpp
+++ Sem. 07/Pract.07/Task10.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-   
+
 constexpr int MAX_SIZE = 100;
 
-int getTheNumnber(int arr1[][MAX_SIZE], int arr2[][MAX_SIZE], int row, int col, int commonDimension) {
+int getTheNumber(int arr1[][MAX_SIZE], int arr2[][MAX_SIZE], int row, int col, int commonDimension) {
     int result = 0;
     for (int i = 0;i < commonDimension;i++) {
         result += arr1[row][i] * arr2[i][col];
@@ -12,9 +12,9 @@ int getTheNumnber(int arr1[][MAX_SIZE], int arr2[][MAX_SIZE], int row, int col, 
 
 void multipliedArrays(int arrMultiplied[][MAX_SIZE], int arr1[][MAX_SIZE], int arr1_row, int arr1_col,
     int arr2[][MAX_SIZE], int arr2_row, int arr2_col) {
-    for (int i = 0;i < arr1_row;i++) {      
+    for (int i = 0;i < arr1_row;i++) {
         for (int j = 0; j < arr2_col;j++) {
-            arrMultiplied[i][j] = getTheNumnber(arr1, arr2, i , j, arr1_col);
+            arrMultiplied[i][j] = getTheNumber(arr1, arr2, i , j, arr1_col);
         }
     }
 }
@@ -55,7 +55,7 @@ void printArray(int arr[][MAX_SIZE], int arr_row, int arr_col) {
 
 void run() {
     int arr1[MAX_SIZE][MAX_SIZE], arr2[MAX_SIZE][MAX_SIZE];
- 
+
     int arr1_row, arr1_col, arr2_row, arr2_col;
 
     std::cin >> arr1_row >> arr1_col;
@@ -63,17 +63,22 @@ void run() {
 
     std::cin >> arr2_row >> arr2_col;
     readArray(arr2, arr2_row, arr2_col);
-    
-    int arrSum[MAX_SIZE][MAX_SIZE], arrMultiplied[MAX_SIZE][MAX_SIZE];
 
-    sumOfArrays(arrSum,arr1,arr2,arr1_row,arr1_col);
-    multipliedArrays(arrMultiplied, arr1, arr1_row, arr1_col, arr2, arr2_row,arr2_col);
-    printArray(arrSum,arr1_row,arr1_col);
-    printArray(arrMultiplied,arr1_row,arr2_col);
+    if (arr1_col == arr2_col && arr1_row == arr2_row) {
+        int arrSum[MAX_SIZE][MAX_SIZE];
+        sumOfArrays(arrSum,arr1,arr2,arr1_row,arr1_col);
+        printArray(arrSum,arr1_row,arr1_col);
+        std::cout << std::endl;
+    }
+
+    if (arr1_col == arr2_row) {
+        int arrMultiplied[MAX_SIZE][MAX_SIZE];
+        multipliedArrays(arrMultiplied, arr1, arr1_row, arr1_col, arr2, arr2_row,arr2_col);
+        printArray(arrMultiplied,arr1_row,arr2_col);
+    }
 }
 
 int main()
 {
     run();
 }
-
