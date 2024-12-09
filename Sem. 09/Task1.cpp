9cf4081331a39315fbdf1d5d6729a17880676fde
Maxim <hristivi8383@gmail.com>
2024-12-09 19:34:47 +0200

    'separating-logic'

--- Sem. 09/Task1.cpp
+++ Sem. 09/Task1.cpp
@@ -4,11 +4,34 @@ constexpr int MAX_SIZE = 1024;
 
 void printArr(const char *arr);
 void findMatch(const char *arr, const char *searched, const char *replacing);
+bool isMatch(const char* searched, const char* text, int& j, const int i);
+void replace(const char* replacing, char* updatedArray, int& updatedArrayIndex, const int searchedWordLength, int& i);
 
 int main() {
   char str[MAX_SIZE] = "i am the best of the best!";
   findMatch(str, "best", "worst");
   return 0;
+}
+
+bool isMatch(const char* searched, const char* text, int& j, const int i) {
+  j = 0;
+  while(searched[j] != '\0') {
+    if(text[i + j] == '\0' || searched[j] != text[i + j]) {
+      return false;
+      break;
+    }
+    j++;
+  }
+  return true;
+}
+void replace(const char* replacing, char* updatedArray, int& updatedArrayIndex, const int searchedWordLength, int& i) {
+  int replacingWordLength = 0;
+  while(replacing[replacingWordLength] != '\0') {
+    updatedArray[updatedArrayIndex + replacingWordLength] = replacing[replacingWordLength];
+    replacingWordLength ++;
+  }
+  updatedArrayIndex += replacingWordLength;
+  i += searchedWordLength;
 }
 
 void findMatch(const char *arr, const char *searched, const char *replacing) {
@@ -19,33 +42,16 @@ void findMatch(const char *arr, const char *searched, const char *replacing) {
 
   while(arr[i] != '\0') {
     updatedArray[updatedArrayIndex] = arr[i];
-    //ако първата буква от търсената дума съвпада, проверявам и останалите букви
     if(arr[i] == searched[0]) {
-      int j = 0;
-      bool isMatch = true;
-      while(searched[j] != '\0') {
-        if(arr[i + j] == '\0' || searched[j] != arr[i + j]) {
-          isMatch = false;
-          break;
-        }
-        j++;
-      }
-      if(isMatch) {
-        //replace logic
-        int replacingWordLength = 0;
-        while(replacing[replacingWordLength] != '\0') {
-          updatedArray[updatedArrayIndex + replacingWordLength] = replacing[replacingWordLength];
-          replacingWordLength ++;
-        }
-        updatedArrayIndex += replacingWordLength;
-        i += j;
+      int searchedWordLength = 0;
+      if(isMatch(searched, arr, searchedWordLength, i)) {
+        replace(replacing, updatedArray, updatedArrayIndex, searchedWordLength, i);
         continue;
       }
     }
     i++;
     updatedArrayIndex++;
   }
-  
   updatedArray[updatedArrayIndex] = '\0';
   printArr(updatedArray);
 }
