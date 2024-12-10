42263bc6ced562e2cca26aad8d7b17d64fea7d71
Slavi Rezashki <57730123+Slavi15@users.noreply.github.com>
2024-12-10 12:04:35 +0200

    Update and rename Task5.cpp to Task05.cpp

--- Sem. 07/Pract.07/Task05.cpp
+++ Sem. 07/Pract.07/Task05.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+
 constexpr int SIZE = 10;
 
 void spiralPrint(const int arr[][SIZE], int size) {
-  int direction = 0;// 0 - right, 1 - down, 2 - left, 3 - up
+  int direction = 0; // 0 - right, 1 - down, 2 - left, 3 - up
   int steps = size;
   int offset = 0;
+  
   while(steps > 0) {
     switch(direction) {
       case 0:
@@ -30,7 +32,9 @@ void spiralPrint(const int arr[][SIZE], int size) {
         }
         break;
     }
+    
     direction++;
+    
     if(direction > 3) {
       offset++;
       direction = 0;
@@ -41,15 +45,18 @@ void spiralPrint(const int arr[][SIZE], int size) {
 int main() {
   int size = 0;
   int array[SIZE][SIZE] = {0};
+  
   do {
     std::cout << "Size of matrix: ";
     std::cin >> size;
   } while(size < 1);
+  
   for(int i = 0;i < size;i++) { 
     for(int j = 0;j < size;j++) {
       std::cin >> array[i][j];
     }
   }
+  
   spiralPrint(array, size);
 
   return 0;
