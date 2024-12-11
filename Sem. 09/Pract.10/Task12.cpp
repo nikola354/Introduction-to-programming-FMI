d5f71451191141c4fb33d1a270fd933689f29c8c
Nikola Manolov <nikola.manolov20@gmail.com>
2024-12-11 11:48:00 +0200

    Update Task12.cpp

--- Sem. 09/Pract.10/Task12.cpp
+++ Sem. 09/Pract.10/Task12.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
+const int UPPER_TO_LOWER_DIFF = 'a'-'A';
+
 void convertString(char* str){
     if(!str) return ;
     
-    const int UPPER_TO_LOWER_DIFF = 'a'-'A';
-
     while(*str){
         if(*str >= 'a' && *str <= 'z') *str = *str - UPPER_TO_LOWER_DIFF;
         else if(*str >='A' && *str <= 'Z') *str = *str + UPPER_TO_LOWER_DIFF;
