aa1eb30813226efeb5fc4afac1736165687cb1f1
nikola354 <nikola.manolov20@gmail.com>
2025-01-10 15:04:22 +0200

    Upload solutions

--- Sem. 12/RecursionQuestions.cpp
+++ Sem. 12/RecursionQuestions.cpp
@@ -2,6 +2,7 @@
 
 // What type of recursion is this, how will it print the array?
 // Array: [1, 2, 3, 4, 5], size: 5
+// Answer: Head recursion => 1 2 3 4 5
 void printArr1(int* arr, size_t size) {
     if (size == 0) {
         return;
@@ -13,6 +14,7 @@ void printArr1(int* arr, size_t size) {
 
 // What type of recursion is this, how will it print the array?
 // Array: [1, 2, 3, 4, 5], size: 5
+// Answer: Tail recursion => 5 4 3 2 1
 void printArr2(int* arr, int size) {
     if (size == 0) {
         return;
@@ -23,6 +25,11 @@ void printArr2(int* arr, int size) {
 }
 
 // What will the function return if n = 5?
+// Answer: the result will always be 0
+// because when N is 1 we should calculate fact with N * fact(--N)
+// --N will give us 0
+// => we have to calculate fact(0) so we return 1
+// but N * 1 is actually 0 * 1, because N is already decremented
 size_t fact1(size_t n) {
     if (n == 0) {
         return 1;
