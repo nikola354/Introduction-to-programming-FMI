53cf00ff86b70ef2b398078923776428a08e302a
Miroslav Balev <zevs8@abv.bg>
2025-01-13 23:55:02 +0200

    Resolve Task 07

--- Sem. 11/Pract.12/Task07.cpp
+++ Sem. 11/Pract.12/Task07.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
 using namespace std;
 
+void swapRows(int** matrix, int from, int to);
+
+void swapPlayers(int* players, int from, int to);
+
 void initializeMatrix(int** matrix, int teams, int* players);
+
+int** createMatrix(int teams, int* players);
 
 int getSum(const int* const* matrix, int row, int players);
 
-void sortMatrix(int** matrix, int teams, int* players);
+void printMatrix(const int* const* matrix, int teams, int* players);
 
-void printMatrix(int** matrix, int teams, int* players);
+void reorderMatrix(int** matrix, int currentTeam, int* players);
 
 void enterPoints(int** matrix, int teams, int* players);
+
+void freeMatrix(const int* const* matrix, int rows);
 
 int main() {
     int teams;
     cin >> teams;
 
-    int** matrix = new int* [teams];
     int* players = new int [teams];
 
-    initializeMatrix(matrix, teams, players);
+    int** matrix = createMatrix(teams, players);
 
     printMatrix(matrix, teams, players);
 
     enterPoints(matrix, teams, players);
 
-    for (int i = 0; i < teams; i++) {
-        delete[] matrix[i];
-    }
-
-    delete[] matrix;
+    freeMatrix(matrix, teams);
     delete[] players;
     return 0;
+}
+
+void swapRows(int** matrix, int from, int to) {
+    if (!matrix) {
+        return;
+    }
+
+    int* temp = matrix[from];
+    matrix[from] = matrix[to];
+    matrix[to] = temp;
+}
+
+void swapPlayers(int* players, int from, int to) {
+    if (!players) {
+        return;
+    }
+
+    int tempPl = players[from];
+    players[from] = players[to];
+    players[to] = tempPl;
 }
 
 void initializeMatrix(int** matrix, int teams, int* players) {
@@ -51,6 +74,14 @@ void initializeMatrix(int** matrix, int teams, int* players) {
     }
 }
 
+int** createMatrix(int teams, int* players) {
+    int** matrix = new int* [teams];
+
+    initializeMatrix(matrix, teams, players);
+
+    return matrix;
+}
+
 int getSum(const int* const* matrix, int row, int players) {
     if (!matrix) {
         return 0;
@@ -65,48 +96,34 @@ int getSum(const int* const* matrix, int row, int players) {
     return sum;
 }
 
-void sortMatrix(int** matrix, int teams, int* players) {
+void printMatrix(const int* const* matrix, int teams, int* players) {
     if (!matrix || !players) {
         return;
     }
-
-    for (int i = 0; i < teams - 1; i++) {
-        int sum = getSum(matrix, i, players[i]);
-        int maxIndex = i;
-
-        for (int k = i + 1; k < teams; k++) {
-            int currentSum = getSum(matrix, k, players[k]); 
-
-            if (currentSum > sum) {
-                maxIndex = k;
-            }
-        }
-
-        if (maxIndex != i) {
-            int* temp = matrix[i];
-            matrix[i] = matrix[maxIndex];
-            matrix[maxIndex] = temp;
-
-            int tempPl = players[i];
-            players[i] = players[maxIndex];
-            players[maxIndex] = tempPl;
-        }
-    }
-
-}
-
-void printMatrix(int** matrix, int teams, int* players) {
-    if (!matrix || !players) {
-        return;
-    }
-
-    sortMatrix(matrix, teams, players);
 
     for (int i = 0; i < teams; i++) {
         for (int j = 0; j < players[i]; j++) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
+    }
+}
+
+void reorderMatrix(int** matrix, int currentTeam, int* players) {
+    if (!matrix || !players) {
+        return;
+    }
+
+    for (int i = currentTeam; i > 0; i--) {
+        int sumCurrent = getSum(matrix, i, players[i]);
+        int sumPrevious = getSum(matrix, i - 1, players[i - 1]);
+
+        if (sumCurrent > sumPrevious) {
+            swapRows(matrix, i, i - 1);
+            swapPlayers(players, i, i - 1);
+        } else {
+            break;
+        }
     }
 }
 
@@ -130,6 +147,21 @@ void enterPoints(int** matrix, int teams, int* players) {
         }
 
         matrix[team][player] += points;
+
+        reorderMatrix(matrix, team, players);
+
         printMatrix(matrix, teams, players);
     }
+}
+
+void freeMatrix(const int* const* matrix, int rows) {
+    if (!matrix) {
+        return;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
 }
